58de46c6ad3978c8ea387a35d983960d9cbe2cb3
RemyCo <remy.cqrd@gmail.com>
2020-01-21 17:25:06 +0100

    Change Config

--- src/main/java/com/config/JDBCConfigurationSol1.java
+++ src/main/java/com/config/JDBCConfigurationSol1.java
@@ -19,22 +19,6 @@ import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 public class JDBCConfigurationSol1 {
 	
 	
-	public static String url;
-	@Value("${jdbc.url}")
-	public void setUrl(String string) {
-		url = string;
-	}
-	public static String user;
-	@Value("${jdbc.user}")
-	public void setUser(String string) {
-		user = string;
-	}
-	public static String password;
-	@Value("${jdbc.password}")
-	public void setPassword(String string) {
-		password = string;
-	} 
-	
 	@Bean
     public static Connection getConnection(){		
 
