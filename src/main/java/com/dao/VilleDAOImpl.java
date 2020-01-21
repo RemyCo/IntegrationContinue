6c7be088c5b257cfa53be2eace890c9f29dceaac
RemyCo <remy.cqrd@gmail.com>
2020-01-21 13:42:38 +0100

    Adding VilleDAO. Removing MissionDAO et Mission/LieuMission

---
+++ src/main/java/com/dao/VilleDAOImpl.java
@@ -0,0 +1,33 @@
+package com.dao;
+
+import java.util.ArrayList;
+
+import com.dto.Ville;
+
+public class VilleDAOImpl implements VilleDAO {
+
+	@Override
+	public ArrayList<Ville> findAllVilles() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Ville> findVilleByCodePostal(String codePostal) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void saveVille(Ville ville) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateVille(String codePostal, Ville ville) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
